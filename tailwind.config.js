c033c68f9ecace3472c22b256f36b32d4b8b8532
CadeBoost <cadeboost@gmail.com>
2022-12-07 22:36:12 -0500

    Updated to a dark theme.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,16 @@ module.exports = {
     "./_includes/**/*.{html,js}",
     "./_layouts/**/*.{html,js}"
   ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'carbon-fiber': "url('/assets/images/bg-carbon-fiber.svg')"
+      },
+      backgroundSize: {
+        'carbon-fiber-size': '12px 11px'
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/typography')
