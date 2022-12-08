74657eb1bf651f3e60caa0e3fac9c42ff6996949
CadeBoost <cadeboost@gmail.com>
2022-12-07 23:16:26 -0500

    Tweaked theme.

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,19 @@ module.exports = {
       },
       backgroundSize: {
         'carbon-fiber-size': '12px 11px'
-      }
+      },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            h2: {
+              color: theme('colors.fuchsia.300')
+            },
+            h3: {
+              color: theme('colors.teal.200')
+            }
+          }
+        }
+      })
     },
   },
   plugins: [
